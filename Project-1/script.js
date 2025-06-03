68268afeedd2559c2f5e29a92e4aa23395f85d22
nguyenvu-laptop <nguye@LAPTOP>
2025-06-04 00:24:14 +0700

    Project-1-Checking Order

--- Project-1/script.js
+++ Project-1/script.js
@@ -109,10 +109,23 @@ function addEventListener() {
         item.addEventListener('dragenter', dragEnter);
         item.addEventListener('dragleave', dragLeave);
     });
-
-    
-
 }
+
+check.addEventListener('click', checkOrder);
+
+function checkOrder() {
+    listItems.forEach((listItem, index) => {
+        const personName = listItem.querySelector('.draggable').innerText.trim();
+
+        if (personName !== richestPeople[index]) {
+            listItem.classList.add('wrong');
+        } else {
+            listItem.classList.remove('wrong');
+            listItem.classList.add('right');
+        }
+    });
+}
+
 
 /*
 
